9c42355b62dd7b70e97f3165d57bdfc7e94fc996
zahid <zahidale@hotmail.com>
2018-08-27 13:27:00 -0300

    search bar

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,23 @@
-alert('hola');
+require('sweetalert2')
+
+      // Menu-toggle button
+
+      $(document).ready(function() {
+            $(".menu-icon").on("click", function() {
+                  $("nav ul").toggleClass("showing");
+            });
+      });
+
+      // Scrolling Effect
+
+      $(window).on("scroll", function() {
+            if($(window).scrollTop()) {
+                  $('nav').addClass('black');
+                  $('.links').addClass('white')
+            }
+
+            else {
+                  $('nav').removeClass('black');
+                  $('.links').removeClass('white')
+            }
+      })
